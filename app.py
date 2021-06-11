6d72697ffd214d819f8816f8cff253482b5e1eda
Benaya Trabelsi <benaya7@gmail.com>
2021-06-11 18:45:12 +0300

    .

--- app.py
+++ app.py
@@ -34,7 +34,8 @@ app.config['JWT_COOKIE_SECURE'] = True
 
 app.config['MONGODB_SETTINGS'] = {
     'db': 'moviesDB',
-    'host': "mongodb+srv://benayat:{password}@cluster0.ptwdq.mongodb.net/authDB?retryWrites=true&w=majority".format(password=os.environ.get("MONGODB_PASSWORD")),
+    'host': "mongodb+srv://benayat:{password}@cluster0.ptwdq.mongodb.net/authDB?retryWrites=true&w=majority"
+    .format(password=os.environ.get("MONGODB_PASSWORD")),
     # 'host': 'mongodb://localhost/database_name'
 }
 initialize_db(app)
